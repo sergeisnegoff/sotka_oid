97490e2da73a440ebeb694b87d21b2858781a422
craftpromotionServer <admin@craftpromotion.ru>
2021-12-14 07:22:58 +0000

    izmeneniya

---
+++ app/Imports/ProductUpdateImport.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Product;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\Importable;
+
+class ProductUpdateImport implements ToCollection, WithChunkReading
+{
+    use Importable;
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function collection(Collection $rows)
+    {
+        return new Product([
+            //
+        ]);
+    }
+
+    public function batchSize(): int
+    {
+        return 100;
+    }
+}
