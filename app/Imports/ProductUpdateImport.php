5fe4f7647c69d94dd389ec965777ccddf3b6f2e3
FreeZ0n <go.gamer.fly@gmail.com>
2021-12-14 18:56:07 +0700

    fix

--- app/Imports/ProductUpdateImport.php
+++ app/Imports/ProductUpdateImport.php
@@ -22,43 +22,53 @@ class ProductUpdateImport implements ToCollection {
 
         $ids = [];
 
-        $collection->each(function (Collection $row, int $key) use (&$last_category_index, &$main_category, &$category, &$data, &$ids){
-            if($key < 5){
-                return;
-            }
-
-            if(is_null($row[7] ?? null)){
-                if($last_category_index + 1 === $key){
-                    $main_category = $category;
+        $collection->each(
+            function (Collection $row, int $key) use (
+                &$last_category_index,
+                &$main_category,
+                &$category,
+                &$data,
+                &$ids
+            ) {
+                if ($key < 5) {
+                    return;
                 }
-                $category = $row[0];
-                $last_category_index = $key;
-            }else{
-                if(!isset($data[$key = "$main_category => $category"])){
-                    $data[$key] = compact('main_category', 'category') + ['items' => []];
+
+                if (is_null($row[7] ?? null)) {
+                    if ($last_category_index + 1 === $key) {
+                        $main_category = $category;
+                    }
+                    $category = $row[0];
+                    $last_category_index = $key;
+                } else {
+                    if (!isset($data[$key = "$main_category => $category"])) {
+                        $data[$key] = compact('main_category', 'category') + ['items' => []];
+                    }
+                    $ids[] = $row[7];
+                    $data[$key]['items'][] = [
+                        'xml_id' => $row[7],
+                        'name' => $row[0],
+                        'cost' => $row[2],
+                        'total' => $row[5],
+                        'multiplicity' => $row[6],
+                        'manufacturer' => $row[8] ?? null,
+                        'image' => $row[9] ?? null,
+                        'filters' => $row[10] ?? null,
+                    ];
                 }
-                $ids[] = $row[7];
-                $data[$key]['items'][] = [
-                    'xml_id' => $row[7],
-                    'name' => $row[0],
-                    'cost' => $row[2],
-                    'total' => $row[5],
-                    'multiplicity' => $row[6],
-                    'manufacturer' => $row[8] ?? null,
-                    'image' => $row[9] ?? null,
-                    'filters' => $row[10] ?? null,
-                ];
             }
-        });
+        );
 
-        ProcessCleanTotalJob::dispatch($ids)
-            ->onConnection(Kernel::CONNECTION_DB)
-            ->onQueue(Kernel::QUEUE_IMPORT);
-
-        foreach($data as $item){
-            ProcessUpdateJob::dispatch($item['main_category'], $item['category'], $item['items'])
+        if (count($data)) {
+            ProcessCleanTotalJob::dispatch($ids)
                 ->onConnection(Kernel::CONNECTION_DB)
                 ->onQueue(Kernel::QUEUE_IMPORT);
+
+            foreach ($data as $item) {
+                ProcessUpdateJob::dispatch($item['main_category'], $item['category'], $item['items'])
+                    ->onConnection(Kernel::CONNECTION_DB)
+                    ->onQueue(Kernel::QUEUE_IMPORT);
+            }
         }
 
     }
