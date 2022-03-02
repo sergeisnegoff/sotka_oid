34da30b39ff56c0b245aa36aff474c9eb16ed8f7
kuliks08 <a.kulikov@craftpromotion.ru>
2022-03-02 12:19:33 +0700

    category and subcategory added

--- app/Http/Controllers/Voyager/ProductsController.php
+++ app/Http/Controllers/Voyager/ProductsController.php
@@ -196,7 +196,6 @@ class ProductsController extends \TCG\Voyager\Http\Controllers\VoyagerBaseContro
     }
 
     public function update(Request $request, $id){
-        dd(1);
         $back = $request->get('__back');
         $request->request->remove('__back');
         $response = parent::update($request, $id);
